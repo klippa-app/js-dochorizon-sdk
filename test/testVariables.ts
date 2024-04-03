c2b1ba683b84fb209a9e433648b77f8f1ed362a0
Els Boerema <els@klippa.com>
2024-04-03 13:20:48 +0200

    added test variables from env

--- test/testVariables.ts
+++ test/testVariables.ts
@@ -1,8 +1,8 @@
 export const TEST_TIMEOUT = 60000;
 export const TEST_TIMEOUT_LOCAL = 7500;
-export const PROMPT_SLUG = "";
-export const PROMPT_JOB = "";
+export const PROMPT_SLUG = process.env.PROMPT_SLUG;
+export const PROMPT_JOB = process.env.PROMPT_JOB;
 
-export const STORAGE_ID = "";
-export const FINANCIAL_JOB = "";
-export const GENERIC_JOB = "";
+export const STORAGE_ID = process.env.STORAGE_ID;
+export const FINANCIAL_JOB = process.env.FINANCIAL_JOB;
+export const GENERIC_JOB = process.env.GENERIC_JOB;
